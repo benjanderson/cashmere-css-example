6c80f3e7ff9e21e017974be1cb3787023e01c191
Ben Anderson <ben.anderson@healthcatalyst.com>
2019-09-30 14:40:02 -0600

    initial commit of webpack

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cashmere-css-example
+
 Example project on how to use the css of https://cashmere.healthcatalyst.net/
+
+## Steps to run app
+
+1. Install Nodejs [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+2. Install git [https://git-scm.com/downloads](https://git-scm.com/downloads)
+3. Run `npm install`
+4. Run `npm start` to start development OR run `npm run build` to build dist
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: { main: './index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
